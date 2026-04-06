ff4164f681c05eadaeea96a0c55e8f8b44c7806f
batman-512 <santanuhaldarone@gmail.com>
2026-04-06 11:37:14 +0530

    added

--- setup.py
+++ setup.py
@@ -3,8 +3,8 @@ from setuptools import find_packages, setup
 setup(
     name = "MlPipeline",
     version= "0.0.0",
-    author="Bappy",
-    author_email="entbappy73@gmail.com",
+    author="SANTANU",
+    author_email="santanuhaldarone@gmail.com",
     packages= find_packages(),
     install_requires=[]
 )
